0b37fff168c4b5dd134ab5c013bc787d437c673a
thor <thor@unknown.home>
2015-06-15 22:32:07 +0100

    commit body.tex etc

---
+++ Makefile
@@ -0,0 +1,4 @@
+MAIN=paper
+include Makefile.general
+GIT_REP=https://github.com/troelleke/PDatalog.git
+include Makefile.git
---
+++ links.mk
@@ -0,0 +1,18 @@
+links:: lstlang0.sty
+lstlang0.sty: $(HOME)/tex/lstlang0.sty
+	ln -sf $< $@
+links:: lstlang1.sty
+lstlang1.sty: $(HOME)/tex/lstlang1.sty
+	ln -sf $< $@
+links:: bibdb.bib
+bibdb.bib: $(HOME)/tex/bibdb.bib
+	ln -sf $< $@
+links:: HySpirit.sty
+HySpirit.sty: $(HOME)/tex/HySpirit.sty
+	ln -sf $< $@
+links:: thormath.sty
+thormath.sty: $(HOME)/tex/thormath.sty
+	ln -sf $< $@
+links:: thorirmath.sty
+thorirmath.sty: $(HOME)/tex/thorirmath.sty
+	ln -sf $< $@
